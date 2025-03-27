30b21138681c368279723b59c49e869bfd808b10
Ajitkumar-P <ajit27041996@yahoo.com>
2025-03-27 10:58:15 +0530

    Added Tailwind and created core-ui child app

--- mfe2/src/App.js
+++ mfe2/src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 
-const App = () => (
-  <div>
-    <h1>MFE2</h1>
-    <nav>
-      <Link to="/mfe2/pageA">Page A</Link> |{" "}
-      <Link to="/mfe2/pageB">Page B</Link>
-    </nav>
-    <Routes>
-      <Route path="pageA" element={<h2>MFE2 - Page A</h2>} />
-      <Route path="pageB" element={<h2>MFE2 - Page B</h2>} />
-    </Routes>
-  </div>
-);
+const App = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>MFE2</h1>
+      <nav>
+        <Link to="/mfe2/pageA">Page A</Link> |{" "}
+        <Link to="/mfe2/pageB">Page B</Link>
+      </nav>
+
+      <button onClick={() => navigate("/mfe1")}>Navigate to MFE 1</button>
+      <Routes>
+        <Route path="pageA" element={<h2>MFE2 - Page A</h2>} />
+        <Route path="pageB" element={<h2>MFE2 - Page B</h2>} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
